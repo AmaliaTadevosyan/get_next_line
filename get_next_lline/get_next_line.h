54373c50c511fc023cbe87b2cd42b79ed5145727
AmaliaTadevosyan <49729766+AmaliaTadevosyan@users.noreply.github.com>
2022-11-26 17:22:14 +0400

    Add files via upload

---
+++ get_next_lline/get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amtadevo <amtadevo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/26 16:11:15 by amtadevo          #+#    #+#             */
+/*   Updated: 2022/11/26 16:11:18 by amtadevo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+size_t	ft_strlen(char *str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
+char	*ft_save(char *save);
+char	*ft_read_save(int fd, char *save);
+char	*ft_get_line(char *save);
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+#endif
